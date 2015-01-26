9cf067bc42422c0f677ca522e815be6cd0e3b165
Antoine Catton <devel@antoine.catton.fr>
2015-01-25 17:47:44 -0700

    Add virtualenv management commands

---
+++ zshrc_functions/wo.zsh
@@ -0,0 +1,17 @@
+wo()
+{
+    cursor="$PWD"
+    until [ "$cursor" = "/" ]
+    do
+        f=$(print "${cursor}/".*/bin/activate) 2> /dev/null
+        if [ -f "$f"  ]
+        then
+            source "$f"
+            return 0
+        fi
+        cursor=$(dirname "$cursor")
+    done
+
+    echo "No virtualenv found"
+    return 127
+}
